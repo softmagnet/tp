e3930426f51e9a94e1dd7caf5deace1953b9de69
Ong Zheng Lin <ongzhenglin1999@gmail.com>
2021-10-12 14:46:23 +0800

    Add schedule command, predicate class for classTiming, parser and relevant tests, also add addressbook.json to git to facilitate future exploratory testing

---
+++ src/test/java/seedu/address/logic/commands/ScheduleCommandTest.java
@@ -0,0 +1,82 @@
+package seedu.address.logic.commands;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
+import static seedu.address.testutil.TypicalPersons.ALICE;
+import static seedu.address.testutil.TypicalPersons.BENSON;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.person.ClassTimingContainsKeywordsPredicate;
+
+/**
+ * Contains integration tests (interaction with the Model) for {@code ScheduleCommand}.
+ */
+public class ScheduleCommandTest {
+    private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+    private Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+
+    @Test
+    public void equals() {
+        ClassTimingContainsKeywordsPredicate firstPredicate =
+                new ClassTimingContainsKeywordsPredicate(Collections.singletonList("first"));
+        ClassTimingContainsKeywordsPredicate secondPredicate =
+                new ClassTimingContainsKeywordsPredicate(Collections.singletonList("second"));
+
+        ScheduleCommand scheduleFirstCommand = new ScheduleCommand(firstPredicate);
+        ScheduleCommand scheduleSecondCommand = new ScheduleCommand(secondPredicate);
+
+        // same object -> returns true
+        assertTrue(scheduleFirstCommand.equals(scheduleFirstCommand));
+
+        // same values -> returns true
+        ScheduleCommand scheduleFirstCommandCopy = new ScheduleCommand(firstPredicate);
+        assertTrue(scheduleFirstCommand.equals(scheduleFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(scheduleFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(scheduleFirstCommand.equals(null));
+
+        // different person -> returns false
+        assertFalse(scheduleFirstCommand.equals(scheduleSecondCommand));
+    }
+
+    @Test
+    public void execute_zeroKeywords_noPersonFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 0);
+        ClassTimingContainsKeywordsPredicate predicate = preparePredicate(" ");
+        ScheduleCommand command = new ScheduleCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Collections.emptyList(), model.getFilteredPersonList());
+    }
+
+    @Test
+    public void execute_multipleKeywords_multiplePersonsFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 2);
+        ClassTimingContainsKeywordsPredicate predicate = preparePredicate("monday tuesday");
+        ScheduleCommand command = new ScheduleCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(ALICE, BENSON), model.getFilteredPersonList());
+    }
+
+    /**
+     * Parses {@code userInput} into a {@code ClassTimingContainsKeywordsPredicate}.
+     */
+    private ClassTimingContainsKeywordsPredicate preparePredicate(String userInput) {
+        return new ClassTimingContainsKeywordsPredicate(Arrays.asList(userInput.split("\\s+")));
+    }
+}
