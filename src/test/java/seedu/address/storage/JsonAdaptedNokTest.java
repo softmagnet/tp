267688d1a742f73e34dc1052919e3c72b8d2729e
bernardwan <bernardwan99@gmail.com>
2021-10-14 19:54:25 +0800

    Fix checkstyle

--- src/test/java/seedu/address/storage/JsonAdaptedNokTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedNokTest.java
@@ -7,6 +7,7 @@ import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.BENSON;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
