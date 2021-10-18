8b9f9c526a6553d5e1c1b438a3a01f136799f2d8
Kevin <skullspine9@gmail.com>
2021-10-18 23:43:51 +0800

    Refactor AddCommandParser to be neater
    - Method is no longer really long

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -50,46 +50,52 @@ public class AddCommandParser implements Parser<AddCommand> {
             argsAfterNok = splitArgs[1];
         }
 
-        ArgumentMultimap argMultimapBeforeNok =
-                ArgumentTokenizer
-                        .tokenize(argsBeforeNok, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
-                                PREFIX_RATE, PREFIX_CLASSTIMING, PREFIX_LOCATION, PREFIX_TAG);
-
-        ArgumentMultimap argMultimapAfterNok =
-                ArgumentTokenizer.tokenize(argsAfterNok, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
-
-        if (!arePrefixesPresent(argMultimapBeforeNok, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE,
-                PREFIX_RATE, PREFIX_CLASSTIMING, PREFIX_LOCATION, PREFIX_EMAIL)
-                || !arePrefixesPresent(argMultimapAfterNok, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)
-                || !argMultimapBeforeNok.getPreamble().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
-        }
-
-        Name name = ParserUtil.parseName(argMultimapBeforeNok.getValue(PREFIX_NAME).get());
-        Phone phone = ParserUtil.parsePhone(argMultimapBeforeNok.getValue(PREFIX_PHONE).get());
-        Email email = ParserUtil.parseEmail(argMultimapBeforeNok.getValue(PREFIX_EMAIL).get());
-        Address address = ParserUtil.parseAddress(argMultimapBeforeNok.getValue(PREFIX_ADDRESS).get());
-        Rate rate = ParserUtil.parseRate(argMultimapBeforeNok.getValue(PREFIX_RATE).get());
-        ClassTiming classTiming = ParserUtil.parseClassTiming(argMultimapBeforeNok.getValue(PREFIX_CLASSTIMING).get());
-        Location location = ParserUtil.parseLocation(argMultimapBeforeNok.getValue(PREFIX_LOCATION).get());
-        Set<Tag> tagList = ParserUtil.parseTags(argMultimapBeforeNok.getAllValues(PREFIX_TAG));
-
-        Nok nok = null;
-
-        if (!argsAfterNok.equals("")) {
-            nok = new Nok(
-                    ParserUtil.parseName(argMultimapAfterNok.getValue(PREFIX_NAME).get()),
-                    ParserUtil.parsePhone(argMultimapAfterNok.getValue(PREFIX_PHONE).get()),
-                    ParserUtil.parseEmail(argMultimapAfterNok.getValue(PREFIX_EMAIL).get()),
-                    ParserUtil.parseAddress(argMultimapAfterNok.getValue(PREFIX_ADDRESS).get())
-            );
-        } else {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
-        }
-
-        Student student = new Student(name, phone, email, address, rate, classTiming, location, nok, tagList);
+        Student student = parseStudent(argsBeforeNok);
+        Nok nok = parseNok(argsAfterNok);
+        student.setNok(nok);
 
         return new AddCommand(student);
+    }
+
+    private Student parseStudent(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer
+                        .tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
+                                PREFIX_RATE, PREFIX_CLASSTIMING, PREFIX_LOCATION, PREFIX_TAG);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE,
+                PREFIX_RATE, PREFIX_CLASSTIMING, PREFIX_LOCATION, PREFIX_EMAIL)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+
+        Name name = ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get());
+        Phone phone = ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get());
+        Email email = ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get());
+        Address address = ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get());
+        Rate rate = ParserUtil.parseRate(argMultimap.getValue(PREFIX_RATE).get());
+        ClassTiming classTiming = ParserUtil.parseClassTiming(argMultimap.getValue(PREFIX_CLASSTIMING).get());
+        Location location = ParserUtil.parseLocation(argMultimap.getValue(PREFIX_LOCATION).get());
+        Set<Tag> tagList = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+
+        return new Student(name, phone, email, address, rate, classTiming, location, null, tagList);
+    }
+
+    private Nok parseNok(String args) throws ParseException {
+        requireNonNull(args);
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_NAME, PREFIX_ADDRESS, PREFIX_PHONE, PREFIX_EMAIL)) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddCommand.MESSAGE_USAGE));
+        }
+
+        return new Nok(
+                ParserUtil.parseName(argMultimap.getValue(PREFIX_NAME).get()),
+                ParserUtil.parsePhone(argMultimap.getValue(PREFIX_PHONE).get()),
+                ParserUtil.parseEmail(argMultimap.getValue(PREFIX_EMAIL).get()),
+                ParserUtil.parseAddress(argMultimap.getValue(PREFIX_ADDRESS).get())
+        );
     }
 
     /**
