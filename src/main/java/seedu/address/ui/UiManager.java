337f28cda6694603de69d15658fb5043a0249bc3
Kevin <skullspine9@gmail.com>
2021-10-30 17:15:25 +0800

    Make UI red

--- src/main/java/seedu/address/ui/UiManager.java
+++ src/main/java/seedu/address/ui/UiManager.java
@@ -67,7 +67,7 @@ public class UiManager implements Ui {
     private static void showAlertDialogAndWait(Stage owner, AlertType type, String title, String headerText,
                                                String contentText) {
         final Alert alert = new Alert(type);
-        alert.getDialogPane().getStylesheets().add("view/BlueTheme.css");
+        alert.getDialogPane().getStylesheets().add("view/RedTheme.css");
         alert.initOwner(owner);
         alert.setTitle(title);
         alert.setHeaderText(headerText);
