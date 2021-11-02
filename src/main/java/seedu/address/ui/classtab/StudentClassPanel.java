630acb16a2d40c4a75b97af2c2454265098d8728
Kevin <skullspine9@gmail.com>
2021-11-02 17:19:36 +0800

    Fix checkstyle

--- src/main/java/seedu/address/ui/classtab/StudentClassPanel.java
+++ src/main/java/seedu/address/ui/classtab/StudentClassPanel.java
@@ -4,10 +4,8 @@ import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
-import javafx.util.Callback;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Student;
 import seedu.address.ui.StudentListViewCell;
