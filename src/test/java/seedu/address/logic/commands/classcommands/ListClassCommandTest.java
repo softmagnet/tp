96023bcbdab64a21a16acb98c24a1a2c44e4eb7f
Stuart Long <chayboon@gmail.com>
2021-11-03 20:37:05 +0800

    Fix checkstyle

--- src/test/java/seedu/address/logic/commands/classcommands/ListClassCommandTest.java
+++ src/test/java/seedu/address/logic/commands/classcommands/ListClassCommandTest.java
@@ -1,16 +1,16 @@
 package seedu.address.logic.commands.classcommands;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.ListCommand;
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.logic.commands.CommandTestUtil.showClassAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalTimestable.getTypicalAddressBook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
 
 class ListClassCommandTest {
     private Model model;
