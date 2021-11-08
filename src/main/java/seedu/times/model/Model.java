f411c3cfc8112bcf4270a77eebbad1ac61638401
Stuart Long <chayboon@gmail.com>
2021-11-08 22:13:33 +0800

    Add javadoc and remove line

--- src/main/java/seedu/times/model/Model.java
+++ src/main/java/seedu/times/model/Model.java
@@ -136,7 +136,13 @@ public interface Model {
     /** Executes update cascade after change of student name for {@code StudentNameList}. */
     void updateClassStudentLists(Name newName, Name oldName);
 
+    /**
+     * Sorts the students based on the comparator c.
+     */
     void sortStudents(Comparator<? super Student> c);
 
+    /**
+     * Sorts the classes based on the comparator c.
+     */
     void sortClasses(Comparator<? super TuitionClass> c);
 }
